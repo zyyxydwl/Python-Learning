a0de81a6d48a08e098524f0bc29d9e7217b31590
zhouyuyao <1330101907@qq.com>
2017-10-18 14:47:21 +0800

    first
    first python file

---
+++ file/first.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#@Time      :2017/10/18 14:42
+#@Author    :zhouyuyao
+#@File      :first.py
+
+
+name = raw_input('Please input your name: ')
+print ("Hello, " +name)
